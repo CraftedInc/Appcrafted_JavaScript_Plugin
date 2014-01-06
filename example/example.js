0e1ac8de7c0ff07f9b658a55ada379044cb5fcfd
James Pasko <jbpasko@gmail.com>
2014-01-05 18:27:20 -0800

    updated the example to use the correct container ID

--- example/example.js
+++ example/example.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     var manager = new AppCrafted("rbw84XwK9nTMwWZecQxr");
-    var assetIDs = ["0", "1", "2", "3"];
+    var assetIDs = ["Vesper", "Sunrise", "Sunset", "Darrington"];
     var i = 0;
-    manager.getAsset("C0zX5M3dvr0KOww3UtXf", assetIDs[i], onLoaded);
+    manager.getAsset("XIQBQpssdnDOic8QEiI1", assetIDs[i], onLoaded);
     $("#next").click(function() {
 	i = ++i % assetIDs.length;
-	manager.getAsset("C0zX5M3dvr0KOww3UtXf", assetIDs[i], onLoaded);
+	manager.getAsset("XIQBQpssdnDOic8QEiI1", assetIDs[i], onLoaded);
     });
     $("#prev").click(function() {
 	i = (i + assetIDs.length - 1) % assetIDs.length;
-	manager.getAsset("C0zX5M3dvr0KOww3UtXf", assetIDs[i], onLoaded);
+	manager.getAsset("XIQBQpssdnDOic8QEiI1", assetIDs[i], onLoaded);
     });
 });
 // This function is called when the AppCrafted manager has the requested Asset.
